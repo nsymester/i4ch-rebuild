a66c7142897da447cbfb73674346d8af46c6cb17
Freal Seven <freal7@yahoo.com>
2019-05-13 11:14:46 +0100

    fix: calculate single trip cover option

--- src/scripts/components/CoverOptions.js
+++ src/scripts/components/CoverOptions.js
@@ -2,15 +2,19 @@
 
 function CoverOptions() {
   // cache DOM
+  const warningText = $('.card-cover-option:nth-of-type(1) .warning-text');
+  const coverOptionPrice = $('.card-cover-option:nth-of-type(1) .card-price');
   // Get single trip price
-  const initialCoverPrice = parseFloat(
-    $('.card-cover-option:nth-of-type(1) .amount').text()
+  const initialCoverPrice = $('.card-cover-option:nth-of-type(1) .amount');
+  const d_initialCoverPrice = parseFloat(
+    initialCoverPrice.text().replace(/\D*/, '')
   ).toFixed(2);
-  let initialSingleTripPrice = parseFloat(
-    $('.initial-single-trip-price')
-      .text()
-      .replace(/\D*/, '')
+
+  const initialSingleTripPrice = $('.initial-single-trip-price');
+  const d_initialSingleTripPrice = parseFloat(
+    initialSingleTripPrice.text().replace(/\D*/, '')
   ).toFixed(2);
+
   let inputValue = '';
   const priceLimit = 119;
   let totalInitialPrice = 0;
@@ -18,32 +22,43 @@ function CoverOptions() {
   let finalPrice = 0;
 
   console.clear();
-  console.log(`cover price ${initialCoverPrice}`);
-  console.log(`cover price ${initialSingleTripPrice}`);
+  console.log(`cover price ${d_initialCoverPrice}`);
+  console.log(`cover price ${d_initialSingleTripPrice}`);
 
   $('.product-options-days-cover').change(function(evt) {
     // get value
     inputValue = parseInt(evt.currentTarget.value);
 
     if (inputValue > 0) {
-      for (let i = 0; i <= inputValue; i = i + 1) {
-        // calculate with intital cover price
-        if (i > 0 && i <= 3) {
-          totalInitialPrice = i * initialCoverPrice;
-          totalSinglePrice = 0;
-        }
+      // calculate with intital cover price
+      if (inputValue > 0 && inputValue <= 3) {
+        totalInitialPrice = inputValue * d_initialCoverPrice;
+        totalSinglePrice = 0;
+      }
 
-        if (i > 3 && finalPrice < priceLimit) {
-          totalSinglePrice = (i - 3) * initialSingleTripPrice;
-        }
+      if (
+        (inputValue > 3 && finalPrice < priceLimit) ||
+        priceLimit < finalPrice
+      ) {
+        totalSinglePrice = (inputValue - 3) * d_initialSingleTripPrice;
       }
     }
-    finalPrice = totalInitialPrice + totalSinglePrice;
+    finalPrice = parseFloat(totalInitialPrice + totalSinglePrice).toFixed(2);
+
+    if (finalPrice > priceLimit) {
+      initialCoverPrice.text(parseFloat(priceLimit).toFixed(2));
+      // change color of price
+      coverOptionPrice.addClass('warning');
+      // show warning text
+      warningText.show();
+    } else {
+      initialCoverPrice.text(finalPrice);
+      warningText.hide();
+      coverOptionPrice.removeClass('warning');
+    }
 
     console.log(`${inputValue} = ${finalPrice}`);
   });
-
-  console.log(initialCoverPrice);
 }
 
 export { CoverOptions };
